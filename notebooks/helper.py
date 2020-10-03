1e5b30078914b40102544aabdb3f52cb3a4c4ad7
gythaogg <gythaoggscat@gmail.com>
2020-10-04 01:31:38 +0200

    Last run on 2020-10-04 01:31

--- notebooks/helper.py
+++ notebooks/helper.py
@@ -17,7 +17,9 @@ def ecdc():
     df = pd.read_json(dumps(json['records']))
     df['dateRep'] = pd.to_datetime(df['dateRep'].astype(str),
                                    format='%d/%m/%Y')
-
+    df['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000'] = pd.to_numeric(
+        df['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000'].
+        fillna(0))
     return df
 
 
@@ -48,7 +50,7 @@ def last_7_days_sum(x):
     return x.iloc[-7:].sum()
 
 
-def overview(selection):
+def overview(selection, compact=True):
     '''
     Returns
     - sum,
@@ -58,10 +60,20 @@ def overview(selection):
     - max
     values for cases and deaths
     '''
-    return selection.sort_values(
-        by=['year', 'month',
-            'day'], ascending=True).groupby("countriesAndTerritories").agg({
+    if not compact:
+        return selection.sort_values(
+            by=['year', 'month', 'day'],
+            ascending=True).groupby("countriesAndTerritories").agg({
                 'cases': ['sum', last_7_days_sum, rolling_avg, latest, 'max'],
                 'deaths': ['sum', last_7_days_sum, rolling_avg, latest, 'max'],
-                'dateRep': ['min', 'max']
+                'dateRep': ['min', 'max'],
+                'Cumulative_number_for_14_days_of_COVID-19_cases_per_100000':
+                latest
+            }).sort_values(by=('cases', 'last_7_days_sum'), ascending=False)
+    else:
+        return selection.sort_values(
+            by=['year', 'month', 'day'],
+            ascending=True).groupby("countriesAndTerritories").agg({
+                'cases': ['sum', last_7_days_sum, rolling_avg, latest],
+                'deaths': ['sum', last_7_days_sum, rolling_avg, latest],
             }).sort_values(by=('cases', 'last_7_days_sum'), ascending=False)
