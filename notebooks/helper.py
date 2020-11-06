3720dc90de584b3d1afb261c8c5c79e69fc66fed
gythaogg <gythaoggscat@gmail.com>
2020-11-06 11:52:46 +0100

    Monthly aggregated deaths

--- notebooks/helper.py
+++ notebooks/helper.py
@@ -5,6 +5,37 @@ WEEKDAYS = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
             'Sunday')
 
 
+def aggregation_rules(fields, datefield):
+    agg_rules = {}
+    agg_rules[datefield] = latest
+    for f in fields:
+        agg_rules[f] = ('max', 'min', 'sum', 'median', latest)
+
+    return agg_rules
+
+
+def group_by_month(df, datefield, fields, include_last=False):
+    agg_rules = aggregation_rules(fields, datefield)
+    grouped = df.groupby(df[datefield].dt.month,
+                         as_index=False).agg(agg_rules).sort_values(
+                             (datefield, 'latest'))
+    if not include_last:
+        return grouped.iloc[:-1]
+
+    return grouped
+
+
+def group_by_week(df, datefield, fields, include_last=False):
+    agg_rules = aggregation_rules(fields, datefield)
+    grouped = df.groupby(df[datefield].dt.isocalendar().week,
+                         as_index=False).agg(agg_rules).sort_values(
+                             (datefield, 'latest'))
+    if not include_last:
+        return grouped.iloc[:-1]
+
+    return grouped
+
+
 def rolling_avg_round(x):
     return np.round(x.iloc[-7:].mean())
 
@@ -52,14 +83,18 @@ def plot_rolling_avg(ax, x, y, roll_days=0, **kwargs):
 
 
 def pretty_plot(ax, **kwargs):
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=90)
     if kwargs.get('xticks'):
-        plt.xticks(*kwargs.get('xticks'), rotation=45)
+        plt.xticks(*kwargs.get('xticks'), rotation=90)
     else:
-        ax.xaxis.set_major_locator(plt.MaxNLocator(20))
+        num_x_locators = kwargs.get('num_x_locators', 20)
+        ax.xaxis.set_major_locator(plt.MaxNLocator(num_x_locators))
 
     if kwargs.get('log'):
         plt.yscale('log')
+
+    if kwargs.get('title'):
+        plt.title(kwargs.get('title'))
 
     plt.legend(loc='best')
     plt.tight_layout()
